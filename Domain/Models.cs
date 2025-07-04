7c7f80a7f5e513e8b63d234fe12516b37332efa1
Peter Robson <145980050+SpaceFalcon15@users.noreply.github.com>
2025-07-04 22:42:12 +0100

    Reapply "Adding the buisness logic with definitions to a museum tour and a city"
    This reverts commit 421a939687dd59acc73edb729c4b53b737c327a0.

---
+++ Domain/Models.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace Domain
+{
+    [XmlRoot("MuseumTours")]
+    public class MuseumTours
+    {
+        [XmlElement("Tour")]
+        public List<MuseumTour> Tours { get; set; } = new List<MuseumTour>();
+    }
+
+    public class MuseumTour
+    {
+        [XmlAttribute("id")]
+        public Guid Id { get; set; } = Guid.NewGuid(); // Generates a new ID for each MuseumTour instance using GUID to make it almost impossible to have a duplicate ID.
+        [XmlAttribute("name")]
+        public string Name { get; set; } = string.Empty; //stops error when null entry is added
+
+        [XmlArray("Cities"), XmlArrayItem("City")]
+        public List<City> Cities { get; set; } = new();
+
+        [XmlArray("Members"), XmlArrayItem("Member")]
+        public List<Member> Members { get; set; } = new();
+    }
+    public class City
+    {
+        [XmlAttribute("id")]
+        public Guid Id { get; set; } = Guid.NewGuid(); // Generates a new ID for each City instance.
+        [XmlAttribute("name")]
+        public string Name { get; set; } = string.Empty;
+        [XmlElement("Description")]
+        public string Description { get; set; } = string.Empty;
+        [XmlElement("Latitude")]
+        public double Latitude { get; set; }
+        [XmlElement("Longitude")]
+        public double Longitude { get; set; }
+
+    }
