1a6433c8a37691be242604773b245a1c375d1831
Peter Robson <145980050+SpaceFalcon15@users.noreply.github.com>
2025-07-04 22:43:41 +0100

    Reapply "Added in serialization for loading and saving"
    This reverts commit 6575175d0535f40f20cc229ceb9a2a68a11b2273.

--- Domain/Models.cs
+++ Domain/Models.cs
@@ -5,7 +5,7 @@ using System.Xml.Serialization;
 namespace Domain
 {
     [XmlRoot("MuseumTours")]
-    public class MuseumTours
+    public class MuseumToursDocumentation
     {
         [XmlElement("Tour")]
         public List<MuseumTour> Tours { get; set; } = new List<MuseumTour>();
