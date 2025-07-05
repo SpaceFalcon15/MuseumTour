a742f922a9b6708d5d829e3a4649452bb8c144a7
Peter Robson <145980050+SpaceFalcon15@users.noreply.github.com>
2025-07-05 22:06:00 +0100

    Added logic for loading in what is currently stored and being able to add a new tour to it.

--- Business/AdminService.cs
+++ Business/AdminService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataStorage;
+using Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,31 @@ using System.Threading.Tasks;
 
 namespace Business
 {
-    class AdminService
+    public class AdminService
     {
+        private readonly XmlStorage _storage;
+        private readonly MuseumToursDocumentation _doc;
+
+        public AdminService(XmlStorage storage)
+        {
+            _storage = storage;
+            _doc = _storage.Load(); // Load existing data or create a new instance if the file does not exist.
+        }
+
+        public MuseumTour AddTour(string name)
+        {
+            var tour = new MuseumTour
+            {
+                Name = name
+            };
+            _doc.Tours.Add(tour); // Add the new tour to the list of tours in the documentation.
+            _storage.Save(_doc); // Save the updated documentation back to the XML file.
+            return tour; // Return the newly created tour.
+        }
+
+        public List<MuseumTour> GetTours()
+        {
+            return _doc.Tours; // Return the list of tours from the documentation.
+        }
     }
 }
