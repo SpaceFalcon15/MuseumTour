b37ca4a8bbb997dc6348cc4bb1f9c9c81df1b9f5
Peter Robson <145980050+SpaceFalcon15@users.noreply.github.com>
2025-07-06 19:22:35 +0100

    Project done?

--- Domain/Visit.cs
+++ Domain/Visit.cs
@@ -12,10 +12,6 @@ namespace Domain
         [XmlAttribute("id")]
         public Guid Id { get; set; } = Guid.NewGuid(); // Generates a new ID for each Visit instance.
         [XmlAttribute("memberId")]
-        public Guid MemberId { get; set; } // ID of the member who made the visit.
-        [XmlAttribute("cityId")]
-        public Guid CityId { get; set; } // ID of the city where the visit took place.
-        [XmlAttribute("museumId")]
         public Guid MuseumId { get; set; } // ID of the museum visited.
         [XmlAttribute("date")]
         public DateTime Date { get; set; } // Date of the visit.
